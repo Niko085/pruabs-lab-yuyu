459fe8a4cb2eb7f9b3ea2a446a43c81e9c1db9d2
Niko085 <nicolas.gajardo.p@usach.cl>
2024-06-19 18:25:46 -0400

    pruebas

---
+++ vehiculo-service/src/main/java/com/example/vehiculo_service/VehiculoServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.vehiculo_service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class VehiculoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculoServiceApplication.class, args);
+	}
+
+}
